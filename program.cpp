70457a2bd9e167004ba4d9b8b24edfd8b91617aa
itstom7 <tahashafqatmalik99@gmail.com>
2024-11-25 14:25:40 +0500

    commented program.cpp

--- program.cpp
+++ program.cpp
@@ -2,12 +2,16 @@
 using namespace std;
 
 int main() {
+    // variables
     int a, b, c;
+    // get all inputs
     cout << "Enter three numbers: ";
     cin >> a >> b >> c;
+    // sum logic
     int max = a;
     if (b > max) max = b;
     if (c > max) max = c;
+    // input logic
     cout << "The maximum number is: " << max << endl;
     return 0;
 }
